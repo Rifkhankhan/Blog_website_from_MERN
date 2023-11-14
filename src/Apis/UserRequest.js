02248f2b5e5359194ff2d6899a72e39405d9aa47
muhammed rifkhan <rifkhanmuhammed17@gmail.com>
2023-11-14 13:17:22 +0530

    forgot password

--- src/Apis/UserRequest.js
+++ src/Apis/UserRequest.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000/' });
-// const API = axios.create({ baseURL: 'https://blog-backend-7kvy.onrender.com' });
+// const API = axios.create({ baseURL: 'http://localhost:5000/' });
+const API = axios.create({ baseURL: 'https://blog-backend-7kvy.onrender.com' });
 
 // export const getUser = (userId) => API.get(`user/${userId}`);
 
